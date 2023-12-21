8c641453053861bc88234a86b2ae4feff5805874
jayashree1002 <137599471+jayashree1002@users.noreply.github.com>
2023-12-21 20:41:52 +0530

    Add files via upload

---
+++ Greatest_of_3_numbers.dart
@@ -0,0 +1,21 @@
+void main()
+{
+var x = 50;
+var y = 100;
+var z = 80;
+
+if(x > y && x > z)
+{
+print("${x} is greater");
+
+}
+else if(y>x && y > z)
+{
+print(" ${y} is greater");
+}
+else
+{
+print("${z} is greater");
+}
+
+}
